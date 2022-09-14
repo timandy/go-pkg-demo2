7f93b5a90c5c0a96b22e57bdfb07a2dc716b8724
TimAndy <xuchonglei@126.com>
2022-09-14 11:43:20 +0800

    print arch

--- hello_test.go
+++ hello_test.go
@@ -1,11 +1,15 @@
 package go_pkg_demo
 
 import (
-	"github.com/stretchr/testify/assert"
+	"fmt"
+	"runtime"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPrintInfo(t *testing.T) {
+	fmt.Println("==========", runtime.GOOS, runtime.GOARCH)
 	info := PrintInfo(33)
 	assert.Equal(t, 33, info)
 }
